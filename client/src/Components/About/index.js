23945dff417f3342652b24e0fba4867b72c83e39
Jared Ruiz <jared707@gmail.com>
2024-05-05 16:23:23 -0500

    Added new css and jsx, will reevaluate layout after more design changes are developed.

--- client/src/Components/About/index.js
+++ client/src/Components/About/index.js
@@ -3,15 +3,26 @@ import React from "react";
 function About() {
     return(
         <>
-        <div id="about">
-            <div className="hero-image">
-                <div id="about-container">
-                    <h1 id="about-text">My Name Is Jared Ruiz</h1>
-                    {/* <p id="about-para"></p> */}
+        <div className="about">
+            <div className="about-background">
+                <div id="about-section1">
+                    <div className="about-mini1">
+                        <h1 id="about-text">My Name Is Jared Ruiz</h1>
+                    </div>
+                   
                 </div>
 
-                </div>
             </div>
+            <div className="about-section2">
+                <div className="about-image">
+                    
+                </div>
+                <div className="about-mini2">
+                    <h2 id="about-h2">I Am A Media Coordinator and Fullstack Developer</h2>
+                </div>
+
+            </div>
+        </div>
         </>
     )
 }
