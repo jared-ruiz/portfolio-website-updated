16ee521e347c4f9ed516b006e7d73cb3b4a66924
Jared Ruiz <jared707@gmail.com>
2024-05-09 17:59:54 -0500

    Added css to fix the flex box issues I was having with the about section 2 portion.

--- client/src/Components/About/index.js
+++ client/src/Components/About/index.js
@@ -11,9 +11,11 @@ function About() {
                     {/* <img src={about_text} id='about_text'></img> */}
                 </div>
                 <div className="about-bg2">
+                    
                     <div className="about-image-container">
                         <img src={about_me_pic} id="profile-pic"></img>
                     </div>
+
                     <div className="background-text">
                         <h2>Background</h2>
                         <p>I am a seasoned media coordinator and front-end developer with a passion for blending creativity and technology to craft immersive digital experiences. With over 3 years of experience in the industry, I have honed my skills across a diverse range of disciplines, from video editing and corporate design to voice-over work and front-end development.</p>
