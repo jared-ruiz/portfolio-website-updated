5a302f8b62815a4ffdf944f10767125e776d8a53
Jared Ruiz <jared707@gmail.com>
2024-06-21 00:43:38 -0500

    Worked on contact page a bit, added jpg's instead of png's to lessen the data needed to load everything up and added icon png's for contact page. Will start styling soon.

--- client/src/Components/Contact/index.js
+++ client/src/Components/Contact/index.js
@@ -1,12 +1,21 @@
 import React from "react";
+import github from '../../Assets/Icons/github-icon.png';
+import linkedin from '../../Assets/Icons/linkedin-icon-03.png';
+import gmail from '../../Assets/Icons/gmail-icon-01.png';
 
 function Contact() {
     return(
         <div>
             <div className="contact">
-                <div className="contact-bg">
-                    
+                <div className="contact-info">
+                    <h2>Email:</h2>
+                    <h2>LinkedIn:</h2>
+                    <h2>Github:</h2>
+                    <img src={github}></img>
+                    <img src={linkedin}></img>
+                    <img src={gmail}></img>
                 </div>
+
             </div>
         </div>
         
