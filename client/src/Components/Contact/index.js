0f082e81f32d561a587381c886aec138621c0a62
Jared Ruiz <jared707@gmail.com>
2024-06-26 10:33:59 -0500

    Began styling the contact page a bit. Deciding on whether or not to do buttons for each contact, or just the resume.

--- client/src/Components/Contact/index.js
+++ client/src/Components/Contact/index.js
@@ -7,15 +7,24 @@ function Contact() {
     return(
         <div>
             <div className="contact">
-                <div className="contact-info">
-                    <h2>Email:</h2>
-                    <h2>LinkedIn:</h2>
-                    <h2>Github:</h2>
-                    <img src={github}></img>
-                    <img src={linkedin}></img>
-                    <img src={gmail}></img>
+                <div className="contact-container">
+                    
+                    <div className="button">
+                        <button>Resume</button>
+                    </div>
+                    
+                    <div className="email-info">
+                        <img src={gmail} id="gmail-logo"></img>
+                        <h2 id="email-h2">Jared707@gmail.com</h2>
+                    </div>
+
                 </div>
 
+                
+                {/* <img src={github}></img>
+                <img src={linkedin}></img>
+                <h2>LinkedIn:</h2>
+                <h2>Github:</h2> */}
             </div>
         </div>
         
