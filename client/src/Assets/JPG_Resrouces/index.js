90890a6f437832f2f2e85756e36aa3101bea48e7
Jared Ruiz <jared707@gmail.com>
2024-07-25 16:45:35 -0500

    Added new design.

--- client/src/Assets/JPG_Resrouces/index.js
+++ client/src/Assets/JPG_Resrouces/index.js
@@ -7,51 +7,60 @@ const ImageArray = [
         },
         {
             index: 1,
-            title: "pondering_hope",
-            src: require('../../Assets/JPG_Images/full_body_thinking.jpg')
+            title: "squat_abstract",
+            src: require('../../Assets/JPG_Images/squatting_01.jpg')
+
         },
         {
             index: 2,
-            title: "woman_on_car",
-            src: require('../../Assets/JPG_Images/woman_on_car.jpg')
+            title: "pondering_hope",
+            src: require('../../Assets/JPG_Images/full_body_thinking.jpg')
+
         },
         {
             index: 3,
-            title: "woman_boots",
-            src: require('../../Assets/JPG_Images/woman_boots.jpg')
+            title: "woman_on_car",
+            src: require('../../Assets/JPG_Images/woman_on_car.jpg')
+
         },
         {
             index: 4,
+            title: "woman_boots",
+            src: require('../../Assets/JPG_Images/woman_boots.jpg')
+
+        },
+        {
+            index: 5,
             title: "retro_self",
             src: require('../../Assets/JPG_Images/me_texture.jpg')
         },
         {
-            index: 5,
-            title: "vintage_tech_poster",
+            index: 6,
+            title: "staircase_red",
             src: require('../../Assets/JPG_Images/staircase_01.jpg')
         },
         {
-            index: 6,
+            index: 7,
             title: "shoes_ad",
             src: require('../../Assets/JPG_Images/shoes_ad.jpg')
         },
         {
-            index: 7,
+            index: 8,
             title: "naja_beauty",
             src: require('../../Assets/JPG_Images/naja_poster.jpg')
         },
         {
-            index: 8,
+            index: 9,
             title: "aragorn",
             src: require('../../Assets/JPG_Images/aragorn_01.jpg')
         },
         {
-            index: 9,
+            index: 10,
             title: "vapor_aesth",
             src: require('../../Assets/JPG_Images/vapor_aesth.jpg')
         },
         {
-            index: 10,
+            index: 11,
             title: "staircase",
             src: require('../../Assets/JPG_Images/vintage-tech-poster.jpg')
         }
