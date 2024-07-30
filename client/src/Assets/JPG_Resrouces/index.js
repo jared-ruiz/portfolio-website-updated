aa2895c1c2ad6ce4789b3a40f704182889cf24c5
Jared Ruiz <jared707@gmail.com>
2024-07-30 01:44:30 -0500

    Added new photo

--- client/src/Assets/JPG_Resrouces/index.js
+++ client/src/Assets/JPG_Resrouces/index.js
@@ -8,59 +8,65 @@ const ImageArray = [
         {
             index: 1,
             title: "squat_abstract",
-            src: require('../../Assets/JPG_Images/squatting_01.jpg')
+            src: require('../../Assets/JPG_Images/squatting_02.jpg')
 
         },
         {
             index: 2,
+            title: "squat_abstract",
+            src: require('../../Assets/JPG_Images/squatting_01.jpg')
+
+        },
+        {
+            index: 3,
             title: "pondering_hope",
             src: require('../../Assets/JPG_Images/full_body_thinking.jpg')
 
         },
         {
-            index: 3,
+            index: 4,
             title: "woman_on_car",
             src: require('../../Assets/JPG_Images/woman_on_car.jpg')
 
         },
         {
-            index: 4,
+            index: 5,
             title: "woman_boots",
             src: require('../../Assets/JPG_Images/woman_boots.jpg')
 
         },
         {
-            index: 5,
+            index: 6,
             title: "retro_self",
             src: require('../../Assets/JPG_Images/me_texture.jpg')
         },
         {
-            index: 6,
+            index: 7,
             title: "staircase_red",
             src: require('../../Assets/JPG_Images/staircase_01.jpg')
         },
         {
-            index: 7,
+            index: 8,
             title: "shoes_ad",
             src: require('../../Assets/JPG_Images/shoes_ad.jpg')
         },
         {
-            index: 8,
+            index: 9,
             title: "naja_beauty",
             src: require('../../Assets/JPG_Images/naja_poster.jpg')
         },
         {
-            index: 9,
+            index: 10,
             title: "aragorn",
             src: require('../../Assets/JPG_Images/aragorn_01.jpg')
         },
         {
-            index: 10,
+            index: 11,
             title: "vapor_aesth",
             src: require('../../Assets/JPG_Images/vapor_aesth.jpg')
         },
         {
-            index: 11,
+            index: 12,
             title: "staircase",
             src: require('../../Assets/JPG_Images/vintage-tech-poster.jpg')
         }
