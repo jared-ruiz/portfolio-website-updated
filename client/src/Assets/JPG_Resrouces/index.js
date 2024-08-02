a40f00f5465cdfb6a630dc6b1a79892a27691e45
Jared Ruiz <jared707@gmail.com>
2024-08-02 00:45:13 -0500

    Added some adjustments to my mobile media query to make the image pop a little bit more, added new design as well.

--- client/src/Assets/JPG_Resrouces/index.js
+++ client/src/Assets/JPG_Resrouces/index.js
@@ -1,5 +1,5 @@
 const ImageArray = [
-    
+
         {
             index: 0,
             title: "pier_glitch",
@@ -7,67 +7,72 @@ const ImageArray = [
         },
         {
             index: 1,
-            title: "squat_abstract",
+            title: "squat_abstract_02",
             src: require('../../Assets/JPG_Images/squatting_02.jpg')
 
         },
         {
             index: 2,
-            title: "squat_abstract",
+            title: "stylized glasses",
+            src: require('../../Assets/JPG_Images/woman_glasses_02.jpg')
+        },
+        {
+            index: 3,
+            title: "squat_abstract_01",
             src: require('../../Assets/JPG_Images/squatting_01.jpg')
 
         },
         {
-            index: 3,
+            index: 4,
             title: "pondering_hope",
             src: require('../../Assets/JPG_Images/full_body_thinking.jpg')
 
         },
         {
-            index: 4,
+            index: 5,
             title: "woman_on_car",
             src: require('../../Assets/JPG_Images/woman_on_car.jpg')
 
         },
         {
-            index: 5,
+            index: 6,
             title: "woman_boots",
             src: require('../../Assets/JPG_Images/woman_boots.jpg')
 
         },
         {
-            index: 6,
+            index: 7,
             title: "retro_self",
             src: require('../../Assets/JPG_Images/me_texture.jpg')
         },
         {
-            index: 7,
+            index: 8,
             title: "staircase_red",
             src: require('../../Assets/JPG_Images/staircase_01.jpg')
         },
         {
-            index: 8,
+            index: 9,
             title: "shoes_ad",
             src: require('../../Assets/JPG_Images/shoes_ad.jpg')
         },
         {
-            index: 9,
+            index: 10,
             title: "naja_beauty",
             src: require('../../Assets/JPG_Images/naja_poster.jpg')
         },
         {
-            index: 10,
+            index: 11,
             title: "aragorn",
             src: require('../../Assets/JPG_Images/aragorn_01.jpg')
         },
         {
-            index: 11,
+            index: 12,
             title: "vapor_aesth",
             src: require('../../Assets/JPG_Images/vapor_aesth.jpg')
         },
         {
-            index: 12,
-            title: "staircase",
+            index: 13,
+            title: "vintage_tech_poster",
             src: require('../../Assets/JPG_Images/vintage-tech-poster.jpg')
         }
 
